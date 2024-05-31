f6344b7fd24715f27688c029855768098ee08549
prarthana-v <p.d.vaghani1729@gmail.com>
2024-05-31 16:12:50 +0530

    dynamic-comp-form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Record from './record'
+
+const App = () => {
+  return (
+    <div>
+      <Record />
+    </div>
+  )
+}
+
+export default App
---
+++ src/record.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './style.css'
+const Record = () => {
+
+  const [input, setInput] = useState([])
+
+  const addMore = () => {
+    let obj = {
+      id: Date.now(),
+      name: '',
+    }
+    setInput([...input, obj]);
+  }
+
+  const deleteI = (id) => {
+    alert(id)
+    let removeI = input.filter((val) => val.id != id);
+    setInput(removeI);
+  }
+
+  return (
+    <div align="center" className=' pt-5'>
+      <h1 className='text-secondary mb-5 '>Dynamic Component Form</h1>
+      <div className="d-flex justify-content-center box ">
+        <form>
+          <label htmlFor="" className='me-4 fs-5 fw-bold'>
+            Name :- <input type="text" />
+          </label>
+          <label htmlFor="" className='fs-5 fw-bold'>
+            Phone :- <input type="text" />
+          </label>
+        </form>
+        <button className='ms-5 btn btn-secondary' onClick={() => addMore()}>Add</button>
+      </div>
+
+
+
+      {
+        input.map((item, index) => {
+          return (
+            <div className="d-flex justify-content-center py-3 box mt-4" key={index}>
+              <form>
+                <label htmlFor="" className='me-4 fs-5 fw-bold'>
+                  Name :- <input type="text" />
+                </label>
+                <label htmlFor="" className='me-4 fs-5 fw-bold'>
+                  Phone :- <input type="text" />
+                </label>
+              </form>
+              <button className='ms-2 btn btn-danger' onClick={() => deleteI(item.id)}>Delete</button>
+            </div>
+          )
+        })
+      }
+
+    </div >
+  )
+}
+
+export default Record
